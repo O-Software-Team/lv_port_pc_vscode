900ed385b530b8abf8273508a964cdbfbc8579ae
Havana Moon <havana@ekofisk.com>
2022-08-18 20:55:34 -0400

    Intial Oserver menu files

--- main/src/main.c
+++ main/src/main.c
@@ -95,7 +95,7 @@ int main(int argc, char **argv)
 //  lv_example_flex_3();
 //  lv_example_label_1();
 
-  lv_demo_widgets();
+  lv_oserver_gui();
 //  lv_demo_keypad_encoder();
 //  lv_demo_benchmark();
 //  lv_demo_stress();
