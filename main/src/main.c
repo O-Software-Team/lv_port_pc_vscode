e64dfcc750730eb19e934bc81e465d90100678d8
Dan Fields <dan.fields@verizon.net>
2023-01-24 09:26:19 -0500

    O boot screen simulation #26

--- main/src/main.c
+++ main/src/main.c
@@ -60,6 +60,11 @@ static int tick_thread(void *data);
  *      VARIABLES
  **********************/
 
+// Special screen objects used globally
+
+lv_obj_t * boot_screen = NULL;
+lv_obj_t * home_screen = NULL;
+
 /**********************
  *  STATIC PROTOTYPES
  **********************/
@@ -79,30 +84,11 @@ int main(int argc, char **argv)
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
   hal_init();
 
-//  lv_example_switch_1();
-//  lv_example_calendar_1();
-//  lv_example_btnmatrix_2();
-//  lv_example_checkbox_1();
-//  lv_example_colorwheel_1();
-//  lv_example_chart_6();
-//  lv_example_table_2();
-//  lv_example_scroll_2();
-//  lv_example_textarea_1();
-//  lv_example_msgbox_1();
-//  lv_example_dropdown_2();
-//  lv_example_btn_1();
-//  lv_example_scroll_1();
-//  lv_example_tabview_1();
-//  lv_example_tabview_1();
-//  lv_example_flex_3();
-//  lv_example_label_1();
-    main_menu_init();
-    oserver_gui();
-//  lv_demo_keypad_encoder();
-//  lv_demo_benchmark();
-//  lv_demo_stress();
-//  lv_demo_music();
+  display_boot_screen();
 
+  main_menu_init();
+  // Start the Menu
+  oserver_gui();
   while(1) {
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
